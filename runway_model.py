e37e0b413ac84d291b0bfe58f57810b2cda327a4
noisyneuron <hello@playdo.io>
2020-07-01 20:35:30 -0400

    text cleanup

--- runway_model.py
+++ runway_model.py
@@ -4,7 +4,7 @@ import os
 from runway.data_types import number, text, image, file, array
 import random
 import gpt_2_simple as gpt2
-
+import re
 
 setup_options = {
     'checkpoint': file(is_directory=True)
@@ -12,52 +12,61 @@ setup_options = {
 
 @runway.setup(options=setup_options)
 def setup(opts):
-
     msg = '[SETUP] Ran with options: file = {}'
     print(msg.format(opts['checkpoint']))
+
     model = {
         'sess' : gpt2.start_tf_sess(),
+        # 'modelname' : 'QBIGrun1-13700'
         'modelname' : opts['checkpoint']
     }
-    # self.sess = gpt2.start_tf_sess()
-    # self.modelname = opts['checkpoint']
+
     gpt2.load_gpt2(model['sess'], model_name=model['modelname'], model_dir='')
-
-    # model = TextModel(opts)
-    # model = TextModel({'checkpoint': 'QBIGrun1-13700'})
-
     return model
+
 
 
 generate_options = {
     'question': text(),
     'temperature' : number(min=0.0, max=1.0, step=0.01, default=0.8, description='the hotter the crazier'),
-    'length' : number(min=1, max=1024, step=1, default=200, description='output char length'),
-    'samples' : number(min=1, max=5, step=1, default=1, description='number of answers to generate')
+    'length' : number(min=1, max=1024, step=1, default=200, description='output char length')
 }
 
-@runway.command(name = 'generate', inputs = generate_options, outputs = { 'answer': array(text) })
+@runway.command(name = 'generate', inputs = generate_options, outputs = { 'answer': text() })
 def generate(model, args):
 
     print('[GENERATE] Ran with caption value "{}"'.format(args['question']))
 
+    question = args['question']+"\n[QASEP]\n"
+
     txt = gpt2.generate(model['sess'],
           model_dir='',
           model_name=model['modelname'],
-          prefix=args['question'],
+          prefix=question,
           length=args['length'],
           temperature=args['temperature'],
           top_p=0.9,
           top_k=40,
-          nsamples=args['samples'],
-          batch_size=args['samples'],
+          nsamples=1,
+          batch_size=1,
           return_as_list=True
           )
 
-    # return txt
-    # output = model.run_on_input(args['question'])
+    print(txt)
 
-    return { 'answer': txt }
+    answer = txt[0]
+    cleanedAns = 'I am too tired to answer right now. Go away.'
+
+    if answer.startswith(question):
+        answer = answer[len(question):]
+        endIndex = answer.find("<|endoftext|>")
+        if endIndex != -1:
+            cleanedAns = answer[:endIndex]
+        else:
+            cleaned = re.findall(r".+[\.|\?]", answer, re.DOTALL)
+            cleanedAns = cleaned[0]
+
+    return cleanedAns
 
 
 
